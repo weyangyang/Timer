a68eaa841c2cc1aad897acdbd64a1f70004a4d01
liyusheng <liyusheng@xuetangx.com>
2016-11-07 13:49:45 +0800

    add wyy scan

---
+++ WyyScan/src/com/wyy/scan/ScanActivity.java
@@ -0,0 +1,15 @@
+package com.wyy.scan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScanActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scan);
+	}
+
+
+}
